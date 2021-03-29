8ba72a511b3d573f6b185313aa0f22c9e6333c11
Constantin <59709240+qwerox@users.noreply.github.com>
2021-03-29 13:46:15 +0300

    Update narodmon_send.py

--- narodmon_send.py
+++ narodmon_send.py
@@ -9,7 +9,7 @@ code = 'ВАШ СЕКРЕТНЫЙ КОД КАМЕРЫ'
 #camera.start_preview()
 camera.resolution = (1920, 1080) # тут можно скорректировать разрешение снимка
 camera.exposure_mode = 'auto'
-#camera.shutter_speed = 70000 # если снимать в сложных условиях, то можно скорректировать вручную вермя выдержки
+#camera.shutter_speed = 70000 # если снимать в сложных условиях, то можно скорректировать вручную время выдержки
 #camera.rotation = 180 # поворот картинки с камеры
 # Даём камере три секунды на автофокусировку и установку баланса белого
 sleep(1)
@@ -21,7 +21,7 @@ str1 = 'image' + '.jpg'
 # Делаем снимок и сохраняем его на рабочий стол с именем image.jpg, тут не факт, что на рабочий стол, скорее в каталог /
 camera.capture(str1)
 #print(str1)
-foto = open(str1, 'rb')  # some file on local disk
+foto = open(str1, 'rb')  # some file on local disk, ну да, из крона точно /
 headers = {}
 url = 'http://narodmon.ru/post'
 files = {code: (code, foto, "image/jpeg")}
